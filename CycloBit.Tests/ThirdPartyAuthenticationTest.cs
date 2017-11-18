46144be7d4ff6b5929fef008d730f006d81106ff
PROMETHEUS\james <jamescoot@gmail.com>
2017-11-17 19:54:43 -0700

    updated cyclobit tests

---
+++ CycloBit.Tests/ThirdPartyAuthenticationTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+
+namespace CycloBit.Tests
+{
+    public class ThirdPartyAuthenticationTest
+    {
+        [Fact]
+        public void FacebookAuthenticationTest() {
+            // HttpClient client = new HttpClient();
+            // Uri facebookLogin = new Uri("https://graph.facebook.com/oauth/access_token");
+            
+
+
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void GoogleAuthenticationTest() {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void TwitterAuthenticationTest() {
+            throw new NotImplementedException();
+        }
+
+    }
+}

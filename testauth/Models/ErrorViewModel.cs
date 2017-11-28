f42cfb24951e4e4966c55770718898ae69c41c31
James Cootware <jamescoot@gmail.com>
2017-11-27 18:58:42 -0700

    initial commit of the sample test project to analyze the account controller

---
+++ testauth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testauth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
